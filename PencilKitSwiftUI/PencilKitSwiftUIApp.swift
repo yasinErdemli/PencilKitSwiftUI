cc36f4e1d25460c180b9be372d9ad2487cf03b11
Yasin Erdemli <yasinerdemli35@icloud.com>
2023-09-20 03:32:01 +0300

    Initial Commit

---
+++ PencilKitSwiftUI/PencilKitSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PencilKitSwiftUIApp.swift
+//  PencilKitSwiftUI
+//
+//  Created by Yasin Erdemli on 20.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct PencilKitSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
